1a347429d1b0d42b55a45a0e0345c08a311f1b33
Li <mail@e.lixingyong.com>
2023-05-17 00:47:34 +0800

    添加图片灯箱 ts

--- src/module/utils.ts
+++ src/module/utils.ts
@@ -6,10 +6,31 @@ export class Utils {
     const contentElement = document.querySelector(".entry-content");
     const tableElements = contentElement?.querySelectorAll("table");
     tableElements?.forEach((tableElement) => {
+      if (tableElement.parentElement?.classList.contains("table-wrapper")) {
+        return;
+      }
       const tableWrapper = document.createElement("div");
       tableWrapper.classList.add("table-wrapper");
       tableElement.parentNode?.insertBefore(tableWrapper, tableElement);
       tableWrapper.appendChild(tableElement);
+    });
+  }
+
+  @documentFunction()
+  public wrapImageWithBox() {
+    const contentElement = document.querySelector(".entry-content");
+    const imageElements = contentElement?.querySelectorAll("img");
+    imageElements?.forEach((imageElement) => {
+      if (imageElement.classList.contains("gallery-img")) {
+        return;
+      }
+      imageElement.classList.add("gallery-img");
+      const imageWrapper = document.createElement("a");
+      imageWrapper.setAttribute("data-fancybox", "gallery");
+      imageWrapper.setAttribute("href", imageElement.getAttribute("src") || "");
+      imageWrapper.classList.add("image-wrapper");
+      imageElement.parentNode?.insertBefore(imageWrapper, imageElement);
+      imageWrapper.appendChild(imageElement);
     });
   }
 }
