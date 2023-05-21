7b4351fb2ced8c3e067fb572bec635981ff1b2b1
Li <mail@e.lixingyong.com>
2023-05-21 16:50:49 +0800

    重构背景图点击切换事件

--- src/page/index.ts
+++ src/page/index.ts
@@ -1,8 +1,46 @@
-import { documentFunction } from "../main";
+import { documentFunction, sakura } from "../main";
 
 export default class Index {
   @documentFunction(false)
   public registerIndex() {
     console.log("123");
+  }
+
+  /**
+   * 注册背景切换事件
+   */
+  @documentFunction(false)
+  public registerBackgroundChangeEvent() {
+    const backgorundElement = document.querySelector(".bg-change-js") as HTMLImageElement;
+    if (!backgorundElement) {
+      return;
+    }
+    const backgroundNextBotton = document.getElementById("bg-next");
+    const backgroundPrevBotton = document.getElementById("bg-prev");
+    const backgroundLoopSize = (sakura.getThemeConfig("random_image").getValue("rimage_cover_back_num", Number) || 0).valueOf();
+    backgroundNextBotton?.addEventListener("click", () => {
+      let currIndex = Number(backgorundElement.getAttribute("data-currIndex"));
+      if (backgroundLoopSize === 0) {
+        currIndex = Math.ceil(Math.random() * 99); 
+      } else {
+        currIndex = (currIndex + 1) % backgroundLoopSize;
+      }
+      handlerChangeBackground(currIndex);
+    });
+
+    backgroundPrevBotton?.addEventListener("click", () => {
+      let currIndex = Number(backgorundElement.getAttribute("data-currIndex"));
+      if (backgroundLoopSize === 0) {
+        currIndex = Math.ceil(Math.random() * 99); 
+      } else {
+        currIndex = (currIndex - 1 + backgroundLoopSize) % backgroundLoopSize;
+      }
+      handlerChangeBackground(currIndex);
+    });
+    const handlerChangeBackground = (backageImageIndex: number) => {
+      const randomUrl = backgorundElement.getAttribute("data-url");
+      backgorundElement.src = `${randomUrl}&t=${backageImageIndex}`;
+      backgorundElement.setAttribute("data-currIndex", `${backageImageIndex}`);
+    }
   }
 }
