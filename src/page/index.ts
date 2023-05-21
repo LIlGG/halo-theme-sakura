53026cc3584f49a3777f6ac69ebd5420a19a3a20
Li <mail@e.lixingyong.com>
2023-05-21 21:55:36 +0800

    完善背景视频

--- src/page/index.ts
+++ src/page/index.ts
@@ -1,6 +1,8 @@
 import { documentFunction, sakura } from "../main";
 
 export default class Index {
+  videoPlayer: any = undefined;
+
   @documentFunction(false)
   public registerIndex() {
     console.log("123");
@@ -17,11 +19,13 @@ export default class Index {
     }
     const backgroundNextBotton = document.getElementById("bg-next");
     const backgroundPrevBotton = document.getElementById("bg-prev");
-    const backgroundLoopSize = (sakura.getThemeConfig("random_image").getValue("rimage_cover_back_num", Number) || 0).valueOf();
+    const backgroundLoopSize = (
+      sakura.getThemeConfig("random_image").getValue("rimage_cover_back_num", Number) || 0
+    ).valueOf();
     backgroundNextBotton?.addEventListener("click", () => {
       let currIndex = Number(backgorundElement.getAttribute("data-currIndex"));
       if (backgroundLoopSize === 0) {
-        currIndex = Math.ceil(Math.random() * 99); 
+        currIndex = Math.ceil(Math.random() * 99);
       } else {
         currIndex = (currIndex + 1) % backgroundLoopSize;
       }
@@ -31,7 +35,7 @@ export default class Index {
     backgroundPrevBotton?.addEventListener("click", () => {
       let currIndex = Number(backgorundElement.getAttribute("data-currIndex"));
       if (backgroundLoopSize === 0) {
-        currIndex = Math.ceil(Math.random() * 99); 
+        currIndex = Math.ceil(Math.random() * 99);
       } else {
         currIndex = (currIndex - 1 + backgroundLoopSize) % backgroundLoopSize;
       }
@@ -41,6 +45,94 @@ export default class Index {
       const randomUrl = backgorundElement.getAttribute("data-url");
       backgorundElement.src = `${randomUrl}&t=${backageImageIndex}`;
       backgorundElement.setAttribute("data-currIndex", `${backageImageIndex}`);
+    };
+  }
+
+  /**
+   * 注册背景视频功能
+   */
+  @documentFunction(false)
+  public registerBackgroundVideo() {
+    const videoContainerElement = document.querySelector(".video-container");
+    if (!videoContainerElement) {
+      return;
     }
+
+    videoContainerElement.insertAdjacentElement("afterbegin", document.createElement("video"));
+    const videoStatusElement = videoContainerElement.querySelector(".video-status") as HTMLDivElement;
+    const focusInfoElement = document.querySelector(".focusinfo") as HTMLDivElement;
+    const videoPlayButtonElement = videoContainerElement.querySelector(".video-play") as HTMLDivElement;
+    const videoPauseButtonElement = videoContainerElement.querySelector(".video-pause") as HTMLDivElement;
+
+    videoPlayButtonElement?.addEventListener("click", async () => {
+      if (!this.videoPlayer) {
+        import("video.js").then((module: any) => {
+          this.videoPlayer = module.default(
+            videoContainerElement.querySelector("video"),
+            {
+              controls: false,
+              controlsBar: false,
+              children: [
+                "MediaLoader"
+              ],
+              autoplay: false,
+              preload: "auto",
+              muted: false,
+              loop: false,
+              sources: [
+                {
+                  src: sakura.getThemeConfig("mainScreen").getValue("bgvideo_url", String)?.toString(),
+                },
+              ],
+            }
+          );
+          this.videoPlayer.ready(() => {
+            videoStatusElement.innerHTML = "正在载入视频 ...";
+            videoStatusElement.style.bottom = "0";
+          });
+
+          this.videoPlayer.on("loadeddata", () => {
+            videoStatusElement.style.bottom = "-100px";
+            focusInfoElement.style.top = "-999px";
+            videoPlayButtonElement.style.display = "none";
+            videoPauseButtonElement.style.display = "block";
+            this.videoPlayer.play();
+          });
+
+          this.videoPlayer.on("play", () => {
+            videoStatusElement.style.bottom = "-100px";
+            focusInfoElement.style.top = "-999px";
+            videoPlayButtonElement.style.display = "none";
+            videoPauseButtonElement.style.display = "block";
+          });
+
+          this.videoPlayer.on("pause", () => {
+            videoStatusElement.innerHTML = "已暂停 ...";
+            videoStatusElement.style.bottom = "0";
+            focusInfoElement.style.top = "0";
+            videoPlayButtonElement.style.display = "block";
+            videoPauseButtonElement.style.display = "none";
+          });
+
+          this.videoPlayer.on("ended", () => {
+            focusInfoElement.style.top = "0";
+            videoStatusElement.style.bottom = "-100px";
+            videoPlayButtonElement.style.display = "block";
+            videoPauseButtonElement.style.display = "none";
+            this.videoPlayer.dispose();
+            videoContainerElement.insertAdjacentElement("afterbegin", document.createElement("video"));
+            this.videoPlayer = undefined;
+          });
+        });
+      } else {
+        this.videoPlayer.play();
+      }
+    });
+
+    videoPauseButtonElement?.addEventListener("click", () => {
+      if (this.videoPlayer) {
+        this.videoPlayer.pause();
+      }
+    });
   }
 }
--- vite.config.ts
+++ vite.config.ts
@@ -33,6 +33,7 @@ export default defineConfig({
       },
     },
     sourcemap: false,
+    chunkSizeWarningLimit: 1024
   },
   resolve: {
     alias: {
