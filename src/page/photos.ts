a77d451cb173c0a25136eb36bef0a78d9d1dfe5c
Li <mail@e.lixingyong.com>
2023-05-23 22:10:28 +0800

    完善相册，由于合理布局均需 jquery，考虑到瀑布流布局已经足够完善，因此暂时移除合理布局

--- src/page/photos.ts
+++ src/page/photos.ts
@@ -1,8 +1,8 @@
-import { documentFunction } from "../main";
+import { documentFunction, sakura } from "../main";
 
 export default class Photos {
   /**
-   * 注册经典布局相册
+   * TODO 注册经典布局相册，暂时未找到 vanilla js 的版本，暂不实现。
    *
    * @see http://miromannino.github.io/Justified-Gallery
    */
@@ -11,6 +11,8 @@ export default class Photos {
 
   /**
    * 注册瀑布流布局相册
+   *
+   * @see https://isotope.metafizzy.co/
    */
   @documentFunction(false)
   public registerMasonryLayout() {
@@ -31,14 +33,31 @@ export default class Photos {
       galleryElement?.querySelectorAll("img.lazyload").forEach((img) => {
         img.addEventListener("load", () => {
           galleryLayout.layout();
-          masonryContainerElement.querySelector(".photos-content")?.classList.remove("loading");
         });
+      });
+
+      galleryLayout.once("arrangeComplete", function () {
+        masonryContainerElement.querySelector(".photos-content")?.classList.remove("loading");
       });
 
       // 过滤
       const galleryFilterbarElement = masonryContainerElement?.querySelector("#gallery-filter");
       const galleryFilterbarItemsElement = galleryFilterbarElement?.querySelectorAll("li a");
+      const defaultGroup = sakura.getThemeConfig("photos").getValue("default_group", String)?.valueOf();
       galleryFilterbarItemsElement?.forEach((hrefElement) => {
+        const filter = hrefElement.getAttribute("data-filter");
+
+        // 默认分组
+        if (defaultGroup) {
+          if (filter == `.${defaultGroup}`) {
+            galleryFilterbarItemsElement[0].classList.remove("active");
+            hrefElement.classList.add("active");
+            galleryLayout.arrange({
+              filter: filter,
+            });
+          }
+        }
+
         hrefElement.addEventListener("click", (event) => {
           event.preventDefault();
           if (hrefElement.classList.contains("active")) {
@@ -48,14 +67,11 @@ export default class Photos {
             item.classList.remove("active");
           });
           hrefElement.classList.add("active");
-          const filter = hrefElement.getAttribute("data-filter");
           galleryLayout.arrange({
             filter: filter,
           });
         });
       });
-
-      // TODO 默认组
 
       // TODO 尝试优化
       const gridChangeElements = masonryContainerElement?.querySelectorAll("#grid-changer a");
