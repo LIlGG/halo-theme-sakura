53173ea7228359f6d48cdf4d3913dd708f52e84f
Li <mail@e.lixingyong.com>
2023-05-28 15:55:42 +0800

    增加环境配置处理

--- vite.config.ts
+++ vite.config.ts
@@ -4,36 +4,38 @@ import { fileURLToPath } from "url";
 import path from "path";
 import PurgeIcons from 'vite-plugin-purge-icons';
 
-export default defineConfig({
-  plugins: [
-    PurgeIcons({
-      content: [
-        './templates/*/*.html',
-      ],
-    })
-  ],
-  build: {
-    minify: true,
-    rollupOptions: {
-      input: Object.fromEntries(
-        globSync(["src/main.ts", "src/libs/**/*.*", "src/page/**/*.*"]).map((file) => [
-          path.relative("src", file.slice(0, file.length - path.extname(file).length)),
-          fileURLToPath(new URL(file, import.meta.url)),
-        ])
-      ),
-      output: {
-        format: "es",
-        dir: fileURLToPath(new URL("./templates/assets/dist", import.meta.url)),
-        entryFileNames: "[name].min.js",
-        assetFileNames: (assetInfo) => {
-          if (assetInfo.name && assetInfo.name.endsWith(".css")) {
-            return "css/[name].min.[ext]";
-          }
-          return "[name].min.[ext]";
+export default ({ mode }: { mode: string }) => {
+  return defineConfig({
+    plugins: [
+      PurgeIcons({
+        content: [
+          './templates/*/*.html',
+        ],
+      })
+    ],
+    build: {
+      minify: mode === 'development' ? false : true,
+      rollupOptions: {
+        input: Object.fromEntries(
+          globSync(["src/main.ts", "src/libs/**/*.*", "src/page/**/*.*"]).map((file) => [
+            path.relative("src", file.slice(0, file.length - path.extname(file).length)),
+            fileURLToPath(new URL(file, import.meta.url)),
+          ])
+        ),
+        output: {
+          format: "es",
+          dir: fileURLToPath(new URL("./templates/assets/dist", import.meta.url)),
+          entryFileNames: "[name].min.js",
+          assetFileNames: (assetInfo) => {
+            if (assetInfo.name && assetInfo.name.endsWith(".css")) {
+              return "css/[name].min.[ext]";
+            }
+            return "[name].min.[ext]";
+          },
         },
       },
+      sourcemap: false,
+      chunkSizeWarningLimit: 1024
     },
-    sourcemap: false,
-    chunkSizeWarningLimit: 1024
-  },
-});
+  });
+};
