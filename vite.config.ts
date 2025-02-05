4cc82ae86598ef6dbb78c627b6bbfea39077956f
Takagi <mail@e.lixingyong.com>
2025-02-05 11:07:38 +0800

    fix: resolve issue of duplicate resource references due to imports (#604)

--- vite.config.ts
+++ vite.config.ts
@@ -3,18 +3,32 @@ import { defineConfig } from "vite";
 import { fileURLToPath } from "url";
 import path from "path";
 import PurgeIcons from 'vite-plugin-purge-icons';
+import fs from 'fs';
+import { loadEnv } from "vite";
 
 const THEME_BASE = "/themes/theme-sakura";
 const ASSETS_BASE = `/assets/dist/`;
 
 export default ({ mode }: { mode: string }) => {
+  const env = loadEnv(mode, process.cwd())
+
   return defineConfig({
     plugins: [
       PurgeIcons({
         content: [
           './templates/*/*.html',
         ],
-      })
+      }),
+      {
+        name: 'write-version-plugin',
+        closeBundle() {
+          const version = env.VITE_THEME_VERSION;
+          const yamlFilePath = path.resolve(__dirname, 'theme.yaml');
+          let yamlContent = fs.readFileSync(yamlFilePath, 'utf8');
+          yamlContent = yamlContent.replace(/version:\s*.*$/m, `version: ${version}`);
+          fs.writeFileSync(yamlFilePath, yamlContent, 'utf8');
+        },
+      }
     ],
     base: THEME_BASE + ASSETS_BASE,
     build: {
@@ -29,12 +43,12 @@ export default ({ mode }: { mode: string }) => {
         ),
         output: {
           format: "es",
-          entryFileNames: "[name].min.js",
+          entryFileNames: `[name]-${env.VITE_THEME_VERSION}.min.js`,
           assetFileNames: (assetInfo) => {
             if (assetInfo.name && assetInfo.name.endsWith(".css")) {
-              return "css/[name].min.[ext]";
+              return `css/[name]-${env.VITE_THEME_VERSION}.min.[ext]`;
             }
-            return "[name].min.[ext]";
+            return `[name].min.[ext]`;
           },
         },
       },
